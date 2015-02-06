eb6284e894c3d5f0272d2708a633e38962b00fce
James Simshaw <james.simshaw@gmail.com>
2015-02-06 11:37:12 -0800

    first commit

---
+++ menu.lua
@@ -0,0 +1,20 @@
+awful = require("awful")
+beautiful = require("beautiful")
+menubar = require("menubar")
+
+myawesomemenu = {
+    { "manual", terminal .. " -e man awesome" },
+    { "edit config", editor_cmd .. " " .. awesome.conffile },
+    { "restart", awesome.restart },
+    { "quit", awesome.quit }
+}
+
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "open terminal", terminal }
+                                  }
+                        })
+
+mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
+                                     menu = mymainmenu })
+
+menubar.utils.terminal = terminal
---
+++ screen.lua
@@ -0,0 +1,35 @@
+local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
+
+beautiful.init("~/.config/awesome/themes/default/theme.lua")
+
+local layouts =
+{
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier
+}
+
+if beautiful.wallpaper then
+    for s = 1, screen.count() do
+        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+    end
+end
+
+tags = {
+    names   = {1, 2, 3, 4, 5, 6, 7, 8, 9 },
+    layout  = { layouts[2], layouts[2], layouts[2], layouts[2], layouts[2],
+                layouts[2], layouts[2], layouts[2], layouts[2] }
+}
+
+for s = 1, screen.count() do
+    tags[s] = awful.tag(tags.names, s, tags.layout)
+end
