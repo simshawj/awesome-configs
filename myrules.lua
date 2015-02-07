f76767ae971bc17868f08a12110a147e2ef0efe4
James Simshaw <james.simshaw@gmail.com>
2015-02-06 19:33:00 -0800

    Split rules off to a new file

---
+++ myrules.lua
@@ -0,0 +1,23 @@
+local awful = require("awful")
+awful.rules = require("awful.rules")
+local beautiful = require("beautiful")
+
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = awful.client.focus.filter,
+                     raise = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons } },
+    { rule = { class = "MPlayer" },
+      properties = { floating = true } },
+    { rule = { class = "pinentry" },
+      properties = { floating = true } },
+    { rule = { class = "gimp" },
+      properties = { floating = true } },
+}
+-- }}}
--- rc.lua
+++ rc.lua
@@ -40,31 +40,8 @@ modkey = "Mod4"
 dofile(awful.util.getdir("config") .. "/screen.lua")
 dofile(awful.util.getdir("config") .. "/menu.lua")
 dofile(awful.util.getdir("config") .. "/mywibox.lua")
-
 dofile(awful.util.getdir("config") .. "/keys.lua")
-
--- {{{ Rules
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = clientkeys,
-                     buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
-}
--- }}}
+dofile(awful.util.getdir("config") .. "/myrules.lua")
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
